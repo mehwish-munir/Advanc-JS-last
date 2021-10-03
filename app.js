97eec2aed5f37878d325e314a32760eaa0ba5984
unknown <mehwishmunir786@gmail.com>
2021-10-03 08:51:21 +0500

    done

---
+++ app.js
@@ -0,0 +1,102 @@
+
+//We have an array of cities. Assign each city in different individual variable. hint ['LA', 'new york', 'karachi','islamabad']
+
+// let cities = ['LA', 'new york', 'karachi','islamabad']
+// let [cit1, cit2, cit3, cit4] = ['LA', 'new york', 'karachi','islamabad']
+// console.log(cit1)
+// console.log(cit2)
+// console.log(cit3)
+// console.log(cit4)
+
+
+//We have a student object. Assign each property in different and individual variable in JS {name:'Marry',age:20,rollNo:2}
+// let record = {
+//     name:'Marry',
+//     age:20,
+//     rollNo:2
+// }
+// console.log(record.name)
+// console.log(record.age)
+// console.log(record.rollNo)
+
+//A function is taking 5 arguments and You have to pass it through an array.
+//let arr = [1,2,3,4,5]
+// function showArray(){
+//      let arr = [1,2,3,4,5]
+//      console.log(arr)
+//      }
+//      showArray()
+//Given an array of firstname and an array of last name. First check both array size is equal or not,
+//  if equal then make third new array which has first last name concat value
+//let firstNames = ['maryam', 'hira', 'faiza', 'aisha']
+//let lastNames = ['khan', 'ayub', 'aziz', 'yasin']
+//['maryam khan', 'hira ayub', 'faiza aziz', 'aisha yasin']
+
+// let firstNames = ['maryam', 'hira', 'faiza', 'aisha']
+// let lastNames = ['khan', 'ayub', 'aziz', 'yasin']
+
+
+//  console.log(firstNames[0] + " " + lastNames[0])
+// console.log(`${firstNames[1]} ${lastNames[1]}`)
+// console.log(`${firstNames[2]} ${lastNames[2]}`)
+// console.log(`${firstNames[3]} ${lastNames[3]}`)
+
+//Create an arrow function that takes array as an input and make each element double and then return an array 
+
+// myFunc1 = () => {    return 2+2 }
+//   myFunc1 = () =>   2+2 
+
+// let b = myFunc1();
+// console.log(b)
+
+//Create an arrow function that takes a number and return square of a number
+
+
+//question:
+ //var authenticated=true;
+//var allowed = authenticated ? (alert('you r allowed')) : (alert('you are not allowed'))
+//which alert will run
+
+// You are allowed
+
+//question: `
+ //What is the value of canDrive
+        // var age = 12;
+        // var canDrive = age > 18 ? 'yes' : 'no';
+        // console.log(canDrive)
+      //  answers: ['12', '18', 'yes', 'no']
+     //   which is correct answer
+       // "no"
+
+/*
+question: `*/
+// const dog='snickers'
+//         logdog(dog);
+//         function logdog(dog){
+//             console.log(dog)
+//         }
+//           function go(){
+//              const dog='sunny';
+//              logdog(dog)
+        
+//          }
+//           go()
+        
+        // answers: ['snickers sunny','sunny snickers','snickers snickers','sunny sunny'] 
+
+
+// let text = `He's often called "Johnny"`;
+//  console.log(text)// write the answer here He's often called "Johnny"
+
+ 
+// function welcomeMsg(name = "Everyone") {
+//     let msg = "Welcome " + name
+//     console.log(msg)
+// }
+// welcomeMsg("Faiza")
+//  welcomeMsg()
+//I have called funtion twice , share the answer what is printed each time
+//welcome Faiza
+//welcome everyone
+
+//make a promise that resolves after 5 sec. Use setTimeOut for resolve.
